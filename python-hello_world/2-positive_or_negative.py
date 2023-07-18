cf883d64eb6be061aefcd3bfabf2df1df8cb0e96
Alwala-kun <annealwala7@gmail.com>
2023-07-18 23:30:14 +0000

    defining if statements

---
+++ python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+# YOUR CODE HERE
+print(number)
+if number > 0:
+    print("is positive\n")
+elif number == 0:
+    print("is zero\n")
+else:
+    print("is negative\n")
