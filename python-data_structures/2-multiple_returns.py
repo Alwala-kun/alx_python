511b6c9d249239276eb114dae0065612ee11672b
Alwala-kun <annealwala7@gmail.com>
2023-08-01 15:51:02 +0000

    learning tuples

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,12 @@
+def multiple_returns(sentence):
+    if sentence:
+        length = len(sentence)
+        first = sentence[0]
+    else:
+        length = 0
+        first = None
+    return (length,first)
+
+
+
+
