3ef44616a034d2a9a03c1ab9f19e2d49e3229d67
Alwala-kun <annealwala7@gmail.com>
2023-08-04 15:26:30 +0000

    added more to square class

---
+++ python-classes/1-square.py
@@ -0,0 +1,23 @@
+"""
+This module defines the Square class, which represents a square with a private instance attribute 'size'.
+"""
+class Square:
+    """
+    Square class represents a square with a private instance attribute 'size'.
+    """
+    def __init__(self,size=0):
+        """
+        Constructor for Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        if not isinstance(size,int):
+            raise TypeError("size must be an integer")
+        
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        
+        self.__size = size
+
+
