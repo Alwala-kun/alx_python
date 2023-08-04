f6270b4362baf565aba33261a39a6397b9570cae
Alwala-kun <annealwala7@gmail.com>
2023-08-04 13:11:02 +0000

    searching for largest integer value

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -13,12 +13,5 @@ def best_score(a_dictionary):
     return best_key
 
 
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
-    
     
         
