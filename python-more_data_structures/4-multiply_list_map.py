302e0f72fb9813144bae2fda67e4e3f5e09a5d1b
Alwala-kun <annealwala7@gmail.com>
2023-08-04 13:17:00 +0000

    reducing the number of code lines

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,6 +1,6 @@
 def multiply_list_map(my_list=[], number=0):
-    new_list = list(map(lambda x : x * number, my_list))
-    return new_list
+    return list(map(lambda x : x * number, my_list))
+    
 
 
 
