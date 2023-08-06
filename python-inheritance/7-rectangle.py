14357e7d0be7e4c0f3950948f3031e065271f964
Alwala-kun <annealwala7@gmail.com>
2023-08-06 14:28:30 +0000

    full rectangle

---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,36 @@
+"Defining an empty class"
+class BaseGeometry:
+    "Defining an empty class"
+
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        if type(value)!= int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle using base geometry"""
+    def __init__(self, width, height):
+        """Initialize a new rectangle
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    
+    def area(self):
+        """Calculate area of rectangle"""
+        return self.__height * self.__width
+    
+    def __print__(self):
+        print(f"[Rectangle] {self.__width}/{self.__height}")
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
