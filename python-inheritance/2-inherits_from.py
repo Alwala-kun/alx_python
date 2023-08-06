8a8c6facaf2a548a6b5c07b25c540a185421a957
Alwala-kun <annealwala7@gmail.com>
2023-08-06 12:38:41 +0000

    removing spaces

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -4,16 +4,11 @@ specified class
 """
 def inherits_from(obj, a_class):
     """Function that determines inheritance
-
     Args:
     obj: object
     a_class: specified class
-    
     """
     obj_class = type(obj)
     if issubclass(obj_class, a_class) and obj_class!= a_class:
         return True
     return False
-
-
-
