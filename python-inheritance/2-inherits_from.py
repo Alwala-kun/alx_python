c82ba7cab3bebcd71be69c74f1aa377ba1a2df9e
Alwala-kun <annealwala7@gmail.com>
2023-08-06 12:36:45 +0000

    only sub class of

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,19 @@
+"""A function that determines whether 
+an object is exactly an child class of a 
+specified class
+"""
+def inherits_from(obj, a_class):
+    """Function that determines inheritance
+
+    Args:
+    obj: object
+    a_class: specified class
+    
+    """
+    obj_class = type(obj)
+    if issubclass(obj_class, a_class) and obj_class!= a_class:
+        return True
+    return False
+
+
+
