3c3cd0c3cf0ff1910e794c271a5a5e55fb48addd
Alwala-kun <annealwala7@gmail.com>
2023-08-06 12:18:27 +0000

    Adding doc strings

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,5 +1,18 @@
+"""A function that determines whether 
+an object is exactly an instance of a 
+specified class
+"""
 def is_same_class(obj, a_class):
+    """Function that determines instance of an object
+
+    Args:
+    obj: object
+    a_class: specified class
+    
+    """
     if type(obj) is a_class:
         return True
     return False
+
+
 
