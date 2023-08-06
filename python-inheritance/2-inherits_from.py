fef97c4959959a9e849a2ba94c01ad3298d0be84
Alwala-kun <annealwala7@gmail.com>
2023-08-06 12:41:18 +0000

    removing whitespaces

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -3,12 +3,15 @@ an object is exactly an child class of a
 specified class
 """
 def inherits_from(obj, a_class):
+
+
     """Function that determines inheritance
     Args:
     obj: object
     a_class: specified class
     """
     obj_class = type(obj)
-    if issubclass(obj_class, a_class) and obj_class!= a_class:
+    if issubclass(obj_class, a_class) and obj_class != a_class:
         return True
     return False
+
